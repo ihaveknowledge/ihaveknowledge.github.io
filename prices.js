5498e7fa368e809e1ecf6bf9f90912e3518968fa
Michael Sinclair <ihaveknowledge@yahoo.co.uk>
2019-01-07 11:23:27 +0000

    formatted items on load

--- prices.js
+++ prices.js
@@ -1,4 +1,8 @@
-d3.csv("prices.csv").get(function(error,data){
+var parseDate = d3.timeParse("%m/%d/%Y");
+
+d3.csv("prices.csv")
+  .row(function(d){ return {month: parseDate(d.month), price: Number(d.price.trim().slice(1))}; })
+  .get(function(error,data){
   console.log(data);
 
 })
