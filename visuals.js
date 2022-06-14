969e6507e0ae67e55b18d666ac753c8e52e0cc35
Michael Sinclair <ihaveknowledge@yahoo.co.uk>
2022-06-14 13:50:18 +0100

    Uploaded single JS and D3 test web page
    This is an HMTL file, CSS style sheet and a JS file which has a simple drawing app with some buttons.

---
+++ visuals.js
@@ -0,0 +1,74 @@
+
+// Create variables here
+var myArray = ["red", "blue", "green", "pink", "orange", "yellow"];
+
+var w = window.innerWidth * 0.9;
+var h = window.innerHeight - 230;
+var newShapes = 25;
+var svg = d3.select("#c2").append("svg").attr("width", w).attr("height", h);
+var randomColour = myArray[Math.floor(Math.random()*myArray.length)];
+var circleCount = 0;
+
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+heightOutput.textContent = window.innerHeight;
+widthOutput.textContent = window.innerWidth;
+
+
+function clean_board() {
+    myCircles = d3.selectAll("circle")._groups[0];
+    circleCount = myCircles.length;
+    //console.log(circleCount);
+    for(let i = 0; i < circleCount; i++) {
+      myCircles[i].remove();
+    }
+
+    myRect = d3.selectAll("rect")._groups[0];
+    rectCount = myRect.length;
+    //console.log(circleCount);
+    for(let i = 0; i < rectCount; i++) {
+      myRect[i].remove();
+    }
+}
+
+function draw_circles() {
+  for (let i = 0; i < newShapes; i++) {
+    var randomColour = myArray[Math.floor(Math.random()*myArray.length)];
+    svg.append("circle")
+        .attr("r", Math.random()*100)
+        .attr("cx", Math.random()*w)
+        .attr("cy", Math.random()*h)
+        .style("opacity", 0.2)
+        .style("fill", randomColour);
+  }
+}
+
+
+function draw_squares() {
+  for (let i = 0; i < newShapes; i++) {
+    var randomColour = myArray[Math.floor(Math.random()*myArray.length)];
+    svg.append("rect")
+        .attr("width", Math.random()*200)
+        .attr("height", Math.random()*200)
+        .attr("x", Math.random()*w)
+        .attr("y", Math.random()*h)
+        .style("opacity", 0.2)
+        .style("fill", randomColour);
+
+  }
+}
+
+
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  h = window.innerHeight - 230;
+  widthOutput.textContent = window.innerWidth;
+  w = window.innerWidth * 0.95;
+  svg.attr("height", h);
+  svg.attr("width", w);
+
+}
+
+window.onresize = reportWindowSize;
